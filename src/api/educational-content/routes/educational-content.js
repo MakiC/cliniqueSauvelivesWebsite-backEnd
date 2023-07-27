c77b68fb1f005ef1f464c200d2b3ac7512b5a7d6
Maki CAMARA <camara.maki@gmail.com>
2023-07-27 17:22:28 +0200

    first commit

---
+++ src/api/educational-content/routes/educational-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * educational-content router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::educational-content.educational-content');
---
+++ src/api/educational-content/services/educational-content.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * educational-content service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::educational-content.educational-content');
